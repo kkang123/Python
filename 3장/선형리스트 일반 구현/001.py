088b134153015cdece01d1fe7242751185d635ef
DESKTOP-73JLLPH\USER <one98kr@gmail.com>
2022-10-19 14:42:13 +0900

    선형리스트 일반예제

---
+++ 3장/선형리스트 일반 구현/001.py
@@ -0,0 +1,15 @@
+katok = []
+
+def add_data(friend):
+
+    katok.append(None)
+    KLen = len(katok)
+    katok[KLen - 1] = friend
+
+add_data('영지')
+add_data('서준')
+add_data('효주')
+add_data('사나')
+add_data('유진')
+
+print(katok)
---
+++ 3장/선형리스트 일반 구현/선형리스트.py
@@ -0,0 +1,14 @@
+katok = []
+
+katok.append(None)
+배열길이 = len(katok)
+print(배열길이) # 1
+katok[배열길이 - 1] = '다현' #katok[0]
+print(katok) # ['다현']
+
+
+katok.append(None)
+배열길이 = len(katok)
+print(배열길이) # 2
+katok[배열길이 - 1] = '정연' #katok[1]
+print(katok) #['다현', '정연']
